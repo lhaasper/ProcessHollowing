2ed954ef9aa187ed5205e9c6c75197c553fb380f
lhaasper <48620355+lhaasper@users.noreply.github.com>
2020-07-17 23:31:07 +0200

    Update Hollow-1.cpp

--- Hollow-1.cpp
+++ Hollow-1.cpp
@@ -1,1 +1,56 @@
 
+#include "Windows.h"
+#include "TlHelp32.h"
+#include <iostream>
+#include <iostream>
+#include<Windows.h>
+#include <tchar.h>
+
+
+using namespace std;
+
+void InjectDLL(DWORD proccessId)
+{
+	const char* dllPath = "C:\\Program Files\\WinRAR\\RAR.dll"; //DLL-PATH HERE (can be anywhere)
+	HANDLE hTargetProcess = OpenProcess(PROCESS_ALL_ACCESS, 0, proccessId); //assigns perms
+	if (hTargetProcess)
+	{
+		LPVOID LoadLibAddr = (LPVOID)GetProcAddress(GetModuleHandleA("kernel32.dll"), "LoadLibraryA");
+		LPVOID RemoteString = VirtualAllocEx(hTargetProcess, NULL, strlen(dllPath), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE); //alocates virtual memory
+		WriteProcessMemory(hTargetProcess, RemoteString, dllPath, strlen(dllPath), NULL); //writes memory to the process
+		CreateRemoteThread(hTargetProcess, NULL, NULL, (LPTHREAD_START_ROUTINE)LoadLibAddr, (LPVOID)RemoteString, NULL, NULL); //creates a remote thread to the handle
+		Sleep(1000); //wait
+		exit(0); //exit
+	}
+}
+
+DWORD PID(LPCSTR proc) //getting the windopws PID
+{
+	DWORD pid;
+	HWND Program = FindWindowA(NULL, proc);
+	while (Program == NULL)
+	{
+		Program = FindWindowA(NULL, proc);
+		Sleep(2000);
+		system("cls");
+	}
+	if (Program != NULL)
+	{
+		GetWindowThreadProcessId(Program, &pid); //gets the PID of the definded window
+	}
+	return pid;
+}
+
+int main()
+{
+
+	system("start Notepad.exe");
+	Sleep(900); //wait for notepad to get starded
+	//InjectDLL(PID("Unbenannt - Editor")); /IGNORE - Only for german pc's 
+  
+  // You should add a language switch using an if statement.
+  
+	InjectDLL(PID("Untitled - Notepad")); //- Input any windows process here, you can use: Regedit, Taskmanager, RegASM and much more.
+	return 0;
+
+}
